265f41956233bb2ec2a8e2460594336698590f92
Catelyn Scholl <cjscholl@Catelyns-MacBook-Air.local>
2020-07-13 21:20:22 -0500

    feat: initial react, babel, webpack setup

---
+++ app/client/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Welcome from './components/Welcome';
+
+export const App = () => (
+    <Welcome/>
+)
+
+export default App;
---
+++ app/client/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/teamfeud/'
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    hot: true,
+    port: 8080,
+    publicPath: 'http://localhost:8080/teamfeud/'
+  },
+  module: {
+    rules: [
+        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/index.html' }
+      ],
+    }),
+  ],
+  
+};
