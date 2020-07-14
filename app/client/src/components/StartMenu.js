114533a75e522fafc3beca7c393ae7610727d8ca
cjscholl <cjscholl@Catelyns-MacBook-Air.local>
2020-07-14 00:31:13 -0500

    feat: add styles for start menu

--- app/client/src/components/StartMenu.js
+++ app/client/src/components/StartMenu.js
@@ -3,17 +3,55 @@ import styled from 'styled-components';
 import { testData } from '../constants/testData';
 import { useHistory } from 'react-router-dom';
 
+const StartMenuContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
 const SelectionMenu = styled.ul`
     list-style: none;
+    margin: 0;
 `;
 
 const SelectionItem = styled.li`
     cursor: pointer;
-    border: 1px solid grey;
+    border: 3px solid #11B5E4;
     border-radius: 10px;
     padding: 10px;
     margin: 10px;
-    background-color: ${props => props.isSelected ? 'lightblue' : 'white'};
+    font-family: 'Oswald', sans-serif;
+    background-color: ${props => props.isSelected ? '#11B5E4' : 'white'};
+    color: ${props => props.isSelected ? 'white' : '#11B5E4'};
+    width: 800px;
+    transition: box-shadow .3s;
+
+    &:hover {
+        box-shadow: 0 0 11px rgba(33,33,33,.2); 
+    }
+`;
+
+const StartButton = styled.button`
+    background-color: #53DD6C;
+    border-radius: 10px;
+    font-size: 20px;
+    border: none;
+    color: white;
+    padding: 20px;
+    width: 200px;
+    transition: box-shadow .3s;
+    font-family: 'Oswald', sans-serif;
+
+    &:hover {
+        box-shadow: 0 0 11px rgba(33,33,33,.2); 
+    }
+`;
+
+const StyledInput = styled.input`
+    font-family: 'Oswald', sans-serif;
+    border: 3px solid #FF4D80;
+    padding: 10px;
+    margin: 10px 0;
+    border-radius: 10px;
 `;
 
 export const StartMenu = () => {
@@ -30,14 +68,14 @@ export const StartMenu = () => {
         history.push(`/game/${selectedGameId}/round/1`)
     }
     return (
-        <>
-            <input placeholder="Team 1" onChange={handleTeamSelection('team1')}/>
-            <input placeholder="Team 2" onChange={handleTeamSelection('team2')}/>
+        <StartMenuContainer>
+            <StyledInput placeholder="Team 1" onChange={handleTeamSelection('team1')}/>
+            <StyledInput placeholder="Team 2" onChange={handleTeamSelection('team2')}/>
             <SelectionMenu>
                 {testData.map(game => <SelectionItem isSelected={selectedGameId === game.id} onClick={() => setSelectedGame(game.id)}>{game.gameName}</SelectionItem>)}
             </SelectionMenu>
-            <button onClick={handleStartGame}>Start!</button>
-        </>
+            <StartButton onClick={handleStartGame}>Start!</StartButton>
+        </StartMenuContainer>
     )
 }
 
