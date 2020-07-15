656cda9aea834ddeae26cf267a75b651a8ecdd6f
Josh Reilly <josh.m.reilly@gmail.com>
2020-07-14 23:06:05 -0500

    chore: add health endpoint

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser';
 import express, { Express } from 'express';
 import http from 'http';
+import HealthApi from './api/HealthApi';
 
 const basepath = '/api';
 
@@ -8,5 +9,7 @@ const app: Express = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(`${basepath}/health`, new HealthApi().wire());
 
 http.createServer(app).listen(8090, () => console.log('running on port 8090...'));
