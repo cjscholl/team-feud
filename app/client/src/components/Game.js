dad5e827165254efdce574df00efbaae5267c239
cjscholl <catelynjscholl@gmail.com>
2020-07-14 21:02:01 -0500

    feat: team info component and styles for welcome page

--- app/client/src/components/Game.js
+++ app/client/src/components/Game.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import TeamInfo from './TeamInfo';
 
 export const Game = () => (
   <>
-
+    <TeamInfo teamId="1" teamNumber="1" teamName="Catelyn" teamScore={100} strikes={3} />
   </>
 );
 
